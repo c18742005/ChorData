f646a3554f4cc8c8af1fd637b2bd9a457b521ac5
c18742005 <c18742005@mytudublin.ie>
2022-08-05 13:14:26 +0100

    Updated url to database

--- chordata-client/src/pages/Dashboard/index.jsx
+++ chordata-client/src/pages/Dashboard/index.jsx
@@ -24,13 +24,13 @@ const Dashboard = () => {
 
   // Check width of screen
   useEffect(() => {
-    window.innerWidth < 426 ? setMobileView(true) : setMobileView(false)
+    window.innerWidth < 1025 ? setMobileView(true) : setMobileView(false)
   }, []);
 
   // Check width of screen
   useEffect(() => {
     const handleResize = () => {
-      window.innerWidth < 426 ? setMobileView(true) : setMobileView(false)
+      window.innerWidth < 1025 ? setMobileView(true) : setMobileView(false)
     }
 
     window.addEventListener('resize', handleResize);
@@ -43,64 +43,14 @@ const Dashboard = () => {
   return (
     mobileView === false ? (
       <Box 
-      align="start" 
-      justify="start" 
-      direction="row" 
-      fill
-      gap="small"
-    >
-      <Box align="center" justify="center" fill gap="medium">
-      <Link to="/clients" style={linkStyle}>
-          <DashboardCard 
-            title="Clients" 
-            image={<FontAwesomeIcon icon={faUsers} size='6x'/>} 
-          />
-        </Link>
-        <Link to="/dentals" style={linkStyle}>
-          <DashboardCard 
-            title="Dental Record" 
-            image={<FontAwesomeIcon icon={faTooth} size='6x' />} 
-          />
-        </Link>
-      </Box>
-      <Box align="center" justify="center" fill gap="medium">
-        <Link to="/anaesthetic" style={linkStyle}>
-          <DashboardCard 
-            title="Anaesthetic Monitoring" 
-            image={<FontAwesomeIcon icon={faHeartPulse} size='6x' />} 
-          />
-        </Link>
-        <Link to="/drugs" style={linkStyle}>
-          <DashboardCard 
-            title="Drug Record" 
-            image={<FontAwesomeIcon icon={faPills} size='6x' />} 
-          />
-        </Link>
-      </Box>
-      <Box align="center" justify="center" fill gap="medium">
-        <Link to="/cremations" style={linkStyle}>
-          <DashboardCard 
-             title="Cremations" 
-             image={<FontAwesomeIcon icon={faCross} size='6x' />} 
-          />
-        </Link>
-        <Link to="/xrays" style={linkStyle}>
-          <DashboardCard 
-            title="X-Ray Exposure" 
-            image={<FontAwesomeIcon icon={faRadiation} size='6x' />} 
-          />
-        </Link>
-      </Box>
-    </Box>
-    ) : (
-    <Box 
-      align="start" 
-      justify="start" 
-      direction="column" 
-      fill
-      gap="small"
-    >
-        <Link to="/clients" style={linkStyle}>
+        align="start" 
+        justify="start" 
+        direction="row" 
+        fill
+        gap="small"
+      >
+        <Box align="center" justify="center" fill gap="medium">
+          <Link to="/clients" style={linkStyle}>
             <DashboardCard 
               title="Clients" 
               image={<FontAwesomeIcon icon={faUsers} size='6x'/>} 
@@ -112,6 +62,8 @@ const Dashboard = () => {
               image={<FontAwesomeIcon icon={faTooth} size='6x' />} 
             />
           </Link>
+        </Box>
+        <Box align="center" justify="center" fill gap="medium">
           <Link to="/anaesthetic" style={linkStyle}>
             <DashboardCard 
               title="Anaesthetic Monitoring" 
@@ -124,6 +76,8 @@ const Dashboard = () => {
               image={<FontAwesomeIcon icon={faPills} size='6x' />} 
             />
           </Link>
+        </Box>
+        <Box align="center" justify="center" fill gap="medium">
           <Link to="/cremations" style={linkStyle}>
             <DashboardCard 
               title="Cremations" 
@@ -136,6 +90,52 @@ const Dashboard = () => {
               image={<FontAwesomeIcon icon={faRadiation} size='6x' />} 
             />
           </Link>
+        </Box>
+      </Box>
+    ) : (
+    <Box 
+      align="start" 
+      justify="start" 
+      direction="column" 
+      fill
+      gap="small"
+    >
+      <Link to="/clients" style={linkStyle}>
+          <DashboardCard 
+            title="Clients" 
+            image={<FontAwesomeIcon icon={faUsers} size='6x'/>} 
+          />
+        </Link>
+        <Link to="/dentals" style={linkStyle}>
+          <DashboardCard 
+            title="Dental Record" 
+            image={<FontAwesomeIcon icon={faTooth} size='6x' />} 
+          />
+        </Link>
+        <Link to="/anaesthetic" style={linkStyle}>
+          <DashboardCard 
+            title="Anaesthetic Monitoring" 
+            image={<FontAwesomeIcon icon={faHeartPulse} size='6x' />} 
+          />
+        </Link>
+        <Link to="/drugs" style={linkStyle}>
+          <DashboardCard 
+            title="Drug Record" 
+            image={<FontAwesomeIcon icon={faPills} size='6x' />} 
+          />
+        </Link>
+        <Link to="/cremations" style={linkStyle}>
+          <DashboardCard 
+            title="Cremations" 
+            image={<FontAwesomeIcon icon={faCross} size='6x' />} 
+          />
+        </Link>
+        <Link to="/xrays" style={linkStyle}>
+          <DashboardCard 
+            title="X-Ray Exposure" 
+            image={<FontAwesomeIcon icon={faRadiation} size='6x' />} 
+          />
+        </Link>
       </Box>
     )
   )
